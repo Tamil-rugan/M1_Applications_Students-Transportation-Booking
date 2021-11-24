0838d50534105efd2dc64b678f1ffb2e876dda98
Tamil Arasan M <tamilrugan99@gmail.com>
2021-11-24 14:29:12 +0530

    Update

--- 3_Implementation/inc/main.h
+++ 3_Implementation/inc/main.h
@@ -1,10 +1,26 @@
-#ifndef _FUN_H
-#define _FUN_H
-{
+#ifndef _MAIN_H
+#define _MAIN_H
+
+
+// Defining Structure
+typedef struct mynode {
+	char name[20];
+	char gen[6];
+	int age;
+	struct mynode* link;
+} Node;
+
+Node* start = NULL;
+
 void details(int);
+int seat(int);
+int cal(int, int, int);
 void bill(int, int);
-void main();
-void add_node(char lol[20], char der[6], int b);
-void bill(int y, int j)
+
+// Global variables
+char source[20], des[20], train[40];
+char station[40], cla[40];
+int time1, time2, a[55];
+
+
 #endif
-  }
--- 3_Implementation/inc/struct.h
+++
@@ -1,7 +0,0 @@
-#include "fun.h"
-typedef struct mynode {
-	char name[20];
-	char gen[6];
-	int age;
-	struct mynode* link;
-}
