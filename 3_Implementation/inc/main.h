c88645ed3cfa91b61baa21c45407a8418247ed28
Tamil Arasan M <94286558+Tamil-rugan@users.noreply.github.com>
2021-11-24 21:47:25 +0530

    Update main.h

--- 3_Implementation/inc/main.h
+++ 3_Implementation/inc/main.h
@@ -1,26 +1,12 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef __HEADER_H__
+#define __HEADER_H__
 
-
-// Defining Structure
-typedef struct mynode {
-	char name[20];
-	char gen[6];
-	int age;
-	struct mynode* link;
-} Node;
-
-Node* start = NULL;
+#include <stdio.h>
+#include <stdlib.h>
 
 void details(int);
 int seat(int);
 int cal(int, int, int);
 void bill(int, int);
-
-// Global variables
-char source[20], des[20], train[40];
-char station[40], cla[40];
-int time1, time2, a[55];
-
 
 #endif
